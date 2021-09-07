08ec4579429072f9853ff884c3b73e755bf495e9
johndwatt <jdw23@protonmail.com>
2021-09-07 14:30:49 -0700

    adds project boilerplate to setup server connection

---
+++ server.js
@@ -0,0 +1,20 @@
+// External Modules
+const express = require("express");
+
+// Internal Modules
+
+
+// Instanced Modules
+const app = express();
+
+// Config
+const PORT = 4000;
+
+// Middleware
+
+// Routes
+
+// Bind to PORT
+app.listen(PORT, function(){
+    console.log(`Server is live on port ${PORT}!` )
+});
