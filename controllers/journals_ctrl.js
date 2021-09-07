f0b56f16aa35efefb4ee4c8cf87588e5332fb9ef
johndwatt <jdw23@protonmail.com>
2021-09-07 16:08:37 -0700

    adds working show route for journals

--- controllers/journals_ctrl.js
+++ controllers/journals_ctrl.js
@@ -1,7 +1,17 @@
 
 const index = async function (req, res, next) {
     try {
-        return res.send("Journal route works!");
+        return res.send("Journal index route works!");
+    } catch (error){
+        console.log(error);
+        req.error = error;
+        return next();
+    }
+}
+
+const show = async function (req, res, next) {
+    try {
+        return res.send(`Journal show route works with id: ${req.params.id}`);
     } catch (error){
         console.log(error);
         req.error = error;
@@ -11,4 +21,5 @@ const index = async function (req, res, next) {
 
 module.exports = {
     index,
+    show,
 }
--- routes/journals.js
+++ routes/journals.js
@@ -4,5 +4,6 @@ const controllers = require("../controllers");
 
 // Routes
 router.get("/", controllers.journals_ctrl.index);
+router.get("/:id", controllers.journals_ctrl.show);
 
 module.exports = router;
