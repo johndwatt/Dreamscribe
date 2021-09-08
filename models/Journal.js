28acd5a0d4bf1b6bc8a7b4f4e5e411477fce52e9
johndwatt <jdw23@protonmail.com>
2021-09-08 16:00:17 -0700

    adds schema for journal

---
+++ models/Journal.js
@@ -0,0 +1,29 @@
+const mongoose = require("mongoose");
+
+const journalSchema = new mongoose.Schema(
+    {
+        title: {
+            type: String,
+            required: [true, "You must title your journal entry"],
+        },
+        content: {
+            type: String,
+            required: [true, "You cannot submit an empty journal entry"],   
+        },
+        createdAt: {
+            type: Date,
+            default: Date.now,
+        },
+        isPublic: {
+            type: Boolean,
+            default: false,
+        },
+    },
+    {
+        timestamps: true,
+    }
+);
+
+const Journal = mongoose.model("Journal", journalSchema);
+
+module.exports = Journal;
