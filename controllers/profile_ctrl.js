79e2470a3935b20c60c77841e22a57971d880a09
johndwatt <jdw23@protonmail.com>
2021-09-14 16:05:18 -0700

    adds search functionality to profile page + reset button

--- controllers/profile_ctrl.js
+++ controllers/profile_ctrl.js
@@ -2,13 +2,39 @@ const { User, Journal } = require("../models");
 
 const profileRoute = async function (req, res, next){
     try {
-        const foundUser = await User.findById({ _id: req.params.id });
-        const userJournals = await Journal.find({ userId: foundUser.id }).sort('-createdAt');
-        const context = {
-            thisUser: foundUser,
-            userJournals: userJournals,
-        };
-        return res.render("user/profile", context);
+        if (req.query.search) {
+            query = {
+                $or: [
+                {
+                    title: {
+                        $regex: new RegExp(req.query.search),
+                        $options: "i",
+                    },
+                },
+                {
+                    content: {
+                        $regex: new RegExp(req.query.search),
+                        $options: "i",
+                    },
+                }],
+            };
+            const foundUser = await User.findById({ _id: req.params.id });
+            const userJournals = await Journal.find({ userId: foundUser.id }).find(query).sort('-createdAt');
+            const context = {
+                thisUser: foundUser,
+                userJournals: userJournals,
+            };
+            return res.render("user/profile", context);
+        } else {
+            const foundUser = await User.findById({ _id: req.params.id });
+            const userJournals = await Journal.find({ userId: foundUser.id }).sort('-createdAt');
+            const context = {
+                thisUser: foundUser,
+                userJournals: userJournals,
+            };
+            return res.render("user/profile", context);
+        }
+        
     } catch (error) {
         console.log(error);
         req.error = error;
