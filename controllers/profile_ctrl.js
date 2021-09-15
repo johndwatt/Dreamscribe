89f54b68d8131f5e186fd7c137012f845f10785d
johndwatt <jdw23@protonmail.com>
2021-09-15 11:48:34 -0700

    adds the ability for a user to update their profile info

--- controllers/profile_ctrl.js
+++ controllers/profile_ctrl.js
@@ -42,6 +42,57 @@ const profileRoute = async function (req, res, next){
     }
 }
 
+
+//edit
+const profileEdit = async function (req, res, next) {
+    try {
+        const foundUser = await User.findById({ _id: req.params.id });
+        const context = {
+            userToEdit: foundUser,
+        };
+        return res.render("user/edit", context);
+    } catch (error){
+        console.log(error);
+        req.error = error;
+        return next();
+    }
+}
+
+//update
+const profileUpdate = async function (req, res, next) {
+    try {
+        const foundUser = await User.findById({ _id: req.params.id });
+        const checkUsername = await User.exists({ username: req.body.username });
+        const checkEmail = await User.exists({ email: req.body.email });
+        if (checkUsername === true && foundUser.username !== req.body.username) {
+            error = {
+                message: "This username is already in use. You cannot update your profile to have the same username as another account.",
+                userToEdit: foundUser,
+            }
+            return res.render("user/edit", error);
+        };
+        if (checkEmail === true && foundUser.email !== req.body.email) {
+            error = {
+                message: "This username is already in use. You cannot update your profile to have the same username as another account.",
+                userToEdit: foundUser,
+            }
+            return res.render("user/edit", error);
+        };
+        const updatedUser = await User.findByIdAndUpdate(
+            req.params.id,
+            {$set: req.body}, 
+            {new: true},
+        );
+        return res.redirect(`/profile/${updatedUser.id}`);
+    } catch (error){
+        console.log(error);
+        req.error = error;
+        return next();
+    }
+}
+
 module.exports = {
     profileRoute,
+    profileEdit,
+    profileUpdate,
 }
--- routes/profile.js
+++ routes/profile.js
@@ -4,5 +4,7 @@ const controllers = require("../controllers");
 
 // Routes
 router.get("/profile/:id", controllers.profile_ctrl.profileRoute);
+router.get("/profile/:id/edit", controllers.profile_ctrl.profileEdit);
+router.put("/profile/:id", controllers.profile_ctrl.profileUpdate)
 
 module.exports = router;
